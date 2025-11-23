47edf83314d9fe1e9eff98a1091b3d9a7c011cf7
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 15:07:06 -0600

    Tuples - Tuplas

---
+++ bases/tipos/tuples.js
@@ -0,0 +1,11 @@
+(() => {
+    const hero = ['Doctor Strange', 100];
+    // No se permite asignar esos valores.
+    // hero[0] = 50;
+    // hero[1] = 'IronMan';
+    hero[0] = 'IronMan';
+    hero[1] = 50;
+    console.log(hero);
+})();
+export {};
+//# sourceMappingURL=tuples.js.map
---
+++ bases/tipos/tuples.ts
@@ -0,0 +1,12 @@
+(()=> {
+    const hero: [string, number] = ['Doctor Strange', 100];
+    
+    // No se permite asignar esos valores.
+    // hero[0] = 50;
+    // hero[1] = 'IronMan';
+
+    hero[0] = 'IronMan';
+    hero[1] = 50;
+    
+    console.log(hero);
+})()
