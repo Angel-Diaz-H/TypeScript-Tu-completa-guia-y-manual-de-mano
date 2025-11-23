6db701b3b36ef93e9aaa305026a0b21be5d4ff65
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 14:52:33 -0600

    Arrays - Arreglos

---
+++ bases/tipos/arrays.js
@@ -0,0 +1,10 @@
+(() => {
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // number.push('11');
+    const villian = ['Omega Rojo', 'Dormamu'];
+    villian.forEach(v => console.log(v.toUpperCase()));
+    // console.log(number);
+    // console.log(villian);
+})();
+export {};
+//# sourceMappingURL=arrays.js.map
---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,12 @@
+(()=> {
+    const number:number[] =[1,2,3,4,5,6,7,8,9,10];
+    // number.push('11');
+
+    const villian = ['Omega Rojo', 'Dormamu'];
+
+    villian.forEach(v => console.log(v.toUpperCase()));
+
+    // console.log(number);
+    // console.log(villian);
+    
+})()
