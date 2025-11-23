d393d8e74438d61d6efa08e091091cbd5031bdff
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 17:26:11 -0600

    Funciones básicas

---
+++ bases/funciones/functions.js
@@ -0,0 +1,12 @@
+(() => {
+    const hero = 'Flash';
+    function returnName() {
+        return hero;
+    }
+    const activateBatisignal = () => {
+        return 'Batiseñar activada!';
+    };
+    const heroName = returnName();
+})();
+export {};
+//# sourceMappingURL=functions.js.map
---
+++ bases/funciones/functions.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    const hero: string = 'Flash';
+    
+    function returnName (): string{
+        return hero;
+    }
+
+    const activateBatisignal = (): string =>{
+        return 'Batiseñar activada!';
+    }
+
+    const heroName: string = returnName();
+})()
