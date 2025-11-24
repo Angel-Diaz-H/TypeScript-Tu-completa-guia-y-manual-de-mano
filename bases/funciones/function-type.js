d515cfd0fa71df616d92264056e64acf515174eb
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-23 18:12:42 -0600

    Tipo función

---
+++ bases/funciones/function-type.js
@@ -0,0 +1,16 @@
+(() => {
+    const addNumbers = (a, b) => a + b;
+    const greet = (name) => `Hola ${name}`;
+    const saveTheWorld = () => `El mundo está salvado`;
+    let myFunction;
+    myFunction = 10;
+    console.log(myFunction);
+    myFunction = addNumbers;
+    console.log(myFunction(1, 2));
+    myFunction = greet;
+    console.log(myFunction('Angel'));
+    myFunction = saveTheWorld;
+    console.log(myFunction());
+})();
+export {};
+//# sourceMappingURL=function-type.js.map
