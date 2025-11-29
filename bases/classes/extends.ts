503bb4047f10cd184f2595f8747185b9d3dfa6de
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-28 18:18:32 -0600

    Get y set

--- bases/classes/extends.ts
+++ bases/classes/extends.ts
@@ -3,44 +3,49 @@
         constructor(
             public name: string,
             public realName: string,
-
         ) {
             console.log(`Constructor Avenger llamado!`);
 
         }
-
         protected getFullName() {
             return `${this.name} ${this.realName}`;
         }
-
-        // Se puede usar public, private, protected.
-        // Por defecto es public.
-        // private: Solo se puede usar dentro de la clase.
-        // protected: Se puede usar dentro de la clase y en las clases que hereden de esta.
     };
 
     class Xmen extends Avenger {
-
         constructor(
             name: string,
             realName: string,
             public isMutant: boolean
-
         ) {
             super(name, realName);
             console.log(`Constructor Xmen llamado!`);
+        }
 
+        get fullName() {
+            return `${this.name} - ${this.realName}`;
+        }
+
+        // No retorna nada.
+        // Solo se recibe un argumento.
+        set fullName(name: string){
+            this.name = name;
+
+            if(name.length < 3){
+                throw new Error('El nombre debe de ser mayor de 3 letras');
+            }
         }
 
         getFullNameDesdeXmen() {
             console.log(super.getFullName());
-
         }
     };
 
     const wolverine = new Xmen('Wolverine', 'Logan', true);
-    console.log(wolverine);
-    wolverine.getFullNameDesdeXmen();
-
+    // console.log(wolverine);
+    // wolverine.getFullNameDesdeXmen();
     const nuevoAvenger = new Avenger('Hola', 'Mundo');
+
+    wolverine.fullName = 'An'
+    console.log(wolverine.fullName);
 })();
--- bases/main.js
+++ bases/main.js
@@ -33,14 +33,23 @@
             this.isMutant = isMutant;
             console.log(`Constructor Xmen llamado!`);
         }
+        get fullName() {
+            return `${this.name} - ${this.realName}`;
+        }
+        set fullName(name) {
+            this.name = name;
+            if (name.length < 3) {
+                throw new Error('El nombre debe de ser mayor de 3 letras');
+            }
+        }
         getFullNameDesdeXmen() {
             console.log(super.getFullName());
         }
     }
     ;
     const wolverine = new Xmen('Wolverine', 'Logan', true);
-    console.log(wolverine);
-    wolverine.getFullNameDesdeXmen();
     const nuevoAvenger = new Avenger('Hola', 'Mundo');
+    wolverine.fullName = 'An';
+    console.log(wolverine.fullName);
 })();
 //# sourceMappingURL=main.js.map
