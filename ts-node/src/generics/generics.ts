b71929c686c835369bcb568e6cca1e445fcde9e9
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-29 00:37:24 -0600

    Introducción a los genéricos

---
+++ ts-node/src/generics/generics.ts
@@ -0,0 +1,7 @@
+export const printObject = (argument: any) => {
+    console.log(argument);
+};
+
+export function genericFunction(argument: any): any {
+    return argument;
+};
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,8 +1,11 @@
-import { Hero } from "./classes/Hero";
-// import * as HeroClasses from "./classes/Hero";
-// import powers, {Power} from "./data/powers";
+import { printObject, genericFunction } from "./generics/generics";
 
-const ironman = new Hero('Ironman', 1, 45);
+// printObject('123');
+// printObject(new Date());
+// printObject({ a: 1, b: 2, c: 3 });
+// printObject([1, 2, 3, 4, 5, 6]);
+// printObject('Hola mundo!!!');
 
-console.log(ironman);
-console.log(ironman.power);
+console.log(genericFunction(123.1416).toFixed(2));
+console.log(genericFunction('Hola mundo!!!').toFixed(2));
+
