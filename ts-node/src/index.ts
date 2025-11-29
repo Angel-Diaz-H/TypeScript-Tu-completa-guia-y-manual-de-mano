bebc0ebf7e9a343eac79a9693a64781c949f5253
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-29 00:12:21 -0600

    Export default y exportaciones con alias

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,4 +1,10 @@
-import { Hero } from "./classes/Hero";
+// import { Hero as SuperHero, Hero2 } from "./classes/Hero";
+import * as HeroClasses from "./classes/Hero";
+import powers, {Power} from "./data/powers";
 
-const ironman = new Hero('Ironman', 1, 45);
+const Hero = 123;
+
+const ironman = new HeroClasses.Hero('Ironman', 1, 45);
 console.log(ironman);
+
+console.log(powers);
