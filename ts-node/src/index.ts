c5e70485d22c426ad19a958e544f46f7558ba7c9
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-11-29 13:35:52 -0600

    Ejemplo aplicado de genéricos

---
+++ ts-node/src/index.ts
@@ -0,0 +1,6 @@
+import { getPokemon } from "./generics/get-pokemon";
+
+getPokemon(4)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+    .finally(() => console.log('Finalizó el proceso'));
